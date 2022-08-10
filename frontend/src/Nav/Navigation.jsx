bbb74fd76de66ea3a78484a7aeaf20492f0bc7fc
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-09 18:14:26 -0700

    Nav routes are semi-working

--- frontend/src/Nav/Navigation.jsx
+++ frontend/src/Nav/Navigation.jsx
@@ -1,70 +1,95 @@
-import * as React from "react";
-import * as ReactRouterDOM from "react-router-dom";
-import * as ReactDOM from "react-dom";
-import useContext from "react";
-import { NavLink, Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import {Navbar, NavItem} from "reactstrap";
 import UserContext from "../Users/UserContext";
-import { Navbar, Nav, NavItem } from "reactstrap";
-
-
-
 
 function Navigation({ logout }) {
   const { currentUser } = useContext(UserContext);
-  console.debug("Navigation", "currentUser=", currentUser);
+  const history = useHistory();
 
-  function loggedInNav() {
-    return (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/companies">
-            Companies
-          </NavLink>
-        </li>
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/jobs">
-            Jobs
-          </NavLink>
-        </li>
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/profile">
-            Profile
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/" onClick={logout}>
-            Log out {currentUser.first_name || currentUser.username}
-          </Link>
-        </li>
-      </ul>
-    );
+  const handleLogout = () => {
+    logout();
+    history.push("/");
   }
-
-  function loggedOutNav() {
-    return (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/login">
-            Login
-          </NavLink>
-        </li>
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/signup">
-            Sign Up
-          </NavLink>
-        </li>
-      </ul>
-    );
-  }
-
   return (
-    <nav className="Navigation navbar navbar-expand-md">
-      <Link className="navbar-brand" to="/">
-        Jobly
-      </Link>
-      {currentUser ? loggedInNav() : loggedOutNav()}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/" className="navbar-brand">Jobly</Link>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          {/* <li className="nav-item">
+            <NavLink to="/" exact className="nav-link" activeClassName="active">Home</NavLink>
+          </li> */}
+          <li className="nav-item">
+            <NavLink to="/companies" className="nav-link" activeClassName="active">Companies</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/jobs" className="nav-link" activeClassName="active">Jobs</NavLink>
+          </li>
+          {currentUser ? (
+            <>
+              <li className="nav-item">
+                <NavLink to="/profile" className="nav-link" activeClassName="active">Profile</NavLink>
+              </li>
+              <li className="nav-item">
+                <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link" activeClassName="active">Login</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/signup" className="nav-link" activeClassName="active">Signup</NavLink>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
     </nav>
   );
+  
 }
+
+
+// function Navigation() {
+//   const { currentUser } = useContext(UserContext);
+//   const history = useHistory();
+
+//   const handleLogout = () => {
+//     history.push("/");
+//   }
+
+
+  // return (
+  //   <nav class="Navigation navbar navbar-expand-md">
+  //     <a class="navbar-brand" href="/">Jobly</a>
+  //     <ul class="navbar-nav ml-auto">
+  //       {currentUser ? (
+  //         <>
+  //           <li class="nav-item">
+  //             <NavLink to="/profile" class="nav-link" activeClassName="active">Profile</NavLink>
+  //           </li>
+  //           <li class="nav-item">
+  //             <button class="btn btn-danger" onClick={handleLogout}>Logout</button>
+  //           </li>
+  //         </>
+  //       ) : (
+  //         <>
+  //           <li class="nav-item">
+  //             <NavLink to="/login" class="nav-link" activeClassName="active">Login</NavLink>
+  //           </li>
+  //           <li class="nav-item">
+  //             <NavLink to="/signup" class="nav-link" activeClassName="active">Signup</NavLink>
+  //           </li>
+  //         </>
+  //       )}
+  //     </ul>
+  //   </nav>
+  
+  // )
+//}
+
 
 export default Navigation;
--- frontend/src/Nav/Routes.jsx
+++ frontend/src/Nav/Routes.jsx
@@ -1,5 +1,5 @@
-import React, {useContext} from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import React, { useContext } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Homepage from "../Homepage";
 import CompanyDetails from "../Company/CompanyDetails";
 import CompanyList from "../Company/CompanyList";
@@ -9,23 +9,70 @@ import UserLoginForm from "../Users/UserLoginForm";
 import UserSignupForm from "../Users/UserSignupForm";
 import UserProfileForm from "../Users/UserProfileForm";
 
-function Routes() {
-    const {currentUser} = useContext(UserContext);
-    return (
-        <Router>
-            <Route path="/" exact component={Homepage} />
-            <Route path="/companies" exact component={CompanyList} />
-            <Route path="/companies/:id" exact component={CompanyDetails} />
-            <Route path="/jobs" exact component={JobList} />
-            <Route path="/login" exact component={UserLoginForm} />
-            <Route path="/signup" exact component={UserSignupForm} />
-            <Route path="/profile">{currentUser ? 
-                <Route path="/profile" exact component={UserProfileForm} /> :
-                <Route path="/profile" exact component={UserLoginForm} />
-            }</Route>
-        </Router>
-    );
-}
+function Routes({ login, signup }) {
+    const { currentUser } = useContext(UserContext);
+//   console.debug(
+//     "Routes rendered=", Boolean, 
+//     "currentUser=", currentUser,
+//     "login=", login,
+//     "signup=", signup
+// );
 
+    // return (
+    //         <Switch>
+    //             <Route exact path="/">
+    //                 <Homepage />
+    //             </Route>
+    //             <Route exact path="/companies">
+    //                 <CompanyList />
+    //             </Route> 
+    //             <Route exact path="/companies/:handle">
+    //                 <CompanyDetails />
+    //             </Route>
+    //             <Route exact path="/jobs">
+    //                 <JobList />
+    //             </Route>
+    //             <Route exact path="/login">
+    //                 <UserLoginForm login={login} />
+    //             </Route>
+    //             <Route exact path="/signup">
+    //                 <UserSignupForm signup={signup} />
+    //             </Route>
+    //             <Route exact path="/profile">
+    //                 <UserProfileForm />
+    //             </Route>
+    //         </Switch>
+    // );
+
+    return (
+        <div className="pt-5">
+            <Switch>
+                <Route exact path="/">
+                    <Homepage />
+                </Route>
+                <Route exact path="/login">
+                    <UserLoginForm login={login} />
+                </Route>
+                <Route exact path="/signup">
+                    <UserSignupForm signup={signup} />
+                </Route>
+                <Route exact path="/companies">
+                    <CompanyList />
+                </Route>
+                <Route exact path="/companies/:handle">
+                    <CompanyDetails />
+                </Route>
+                <Route exact path="/jobs">
+                    <JobList />
+                </Route>
+                {/* <Route exact path="/profile">
+                    <UserProfileForm />
+                </Route> */}
+                <Redirect to="/" />
+                </Switch>
+
+        </div>
+    )
+}
 
 export default Routes;
