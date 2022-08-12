974bf4848d68daf0ef8f751227f3fa66134728d1
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-11 21:14:46 -0700

    Hallelujah! Company card page now renders

--- frontend/src/Companies/CompanyCard.js
+++ frontend/src/Companies/CompanyCard.js
@@ -1,39 +1,46 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import JoblyApi from "../JoblyApi";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+// import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import {CardHeader, CardTitle} from "reactstrap";
 
-function CompanyCard() {
-    const [companies, setCompanies] = useState(null);
-    console.debug("CompanyCard", "companies=", companies);
+function CompanyCard({ handle, name, description, logoUrl, numEmployees }) {
+  const history = useHistory();
 
-    useEffect(() => {
-        async function getCompany() {
-            const companies = await JoblyApi.getCompany();
-            setCompanies(companies);
-        }
-        getCompany();
-    }
-    , []);
+  return (
+    // <div className="col-md-4">
+    //     <div className="card mb-4 shadow-sm">
+    //         <div className="card-body">
+    //             <img src={logoUrl} alt={name} className="img-thumbnail" />
+    //             <h5 className="card-title">{name}</h5>
+    //             <p className="card-text">{description}</p>
+    //             <div className="d-flex justify-content-between align-items-center">
+    //                 <div className="btn-group">
+    //                     <Link to={`/companies/${handle}`} className="btn btn-sm btn-outline-secondary">View</Link>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
 
-    return (
-        <div className="CompanyCard">
-        <Link to={`/companies/${companies.handle}`}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <img src={companies.logo_url} alt={companies.name} />
-                    </div>
-                    <div className="col-md-8">
-                        <h2>{companies.name}</h2>
-                        <p>{companies.description}</p>
-                    </div>
-                </div>
-            </div>
-        </Link>
-        </div>  
-    );
+    <Card style={{ width: "18rem" }}>
+    <CardHeader>
+    <Card.Title>{name}</Card.Title>
+    <div>
+      <Card.Img variant="top" img src={logoUrl} />
+      </div>
+      <Card.Body>
+        <Card.Text>
+        {description}
+        </Card.Text>
+        <Button variant="primary">View Job</Button>
+      </Card.Body>
+    </CardHeader>
+    </Card>
+  );
 }
 
-
-
-    export default CompanyCard;
+export default CompanyCard;
