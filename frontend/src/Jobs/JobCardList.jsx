3ce53e9a20a8265f0f611100a2bf5736279e8f34
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-22 20:17:49 -0700

    Deleted redundant starter code

--- frontend/src/Jobs/JobCardList.jsx
+++ frontend/src/Jobs/JobCardList.jsx
@@ -11,51 +11,6 @@ import {
   Row,
 } from "reactstrap";
 
-// function CompanyList() {
-//   const [companies, setCompanies] = useState([]);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     async function getCompanies() {
-//       try {
-//         const companies = await JoblyApi.getCompanies();
-//         setCompanies(companies);
-//       } catch (err) {
-//         setError(err);
-//       }
-//     }
-//     getCompanies();
-//   }, []);
-
-//   if (error) {
-//     return <p> Error: {error.message} </p>;
-//   }
-
-//   return (
-//     <div className="row">
-//       {companies.map((company) => (
-//         <CompanyCard key={company.handle} company={company} />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default CompanyList;
-
-
-// function JobCardList({ jobs }) {
-//   return (
-//     <CardColumns>
-//       {jobs.map((job) => (
-//         <JobCard key={job.id} job={job} />
-//       ))}
-//     </CardColumns>
-    
-
-//   );
-
-// }
-
 
 function JobCardList({ jobs }) {
   return (
