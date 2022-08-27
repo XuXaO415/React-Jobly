d916ac289c939f65f7be3fe8359dcd5a10cca468
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-26 21:51:54 -0700

    Revised user signup form

--- frontend/src/Users/UserSignupForm.jsx
+++ frontend/src/Users/UserSignupForm.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, Component } from "react";
 // https://bobbyhadz.com/blog/react-export-usehistory-was-not-found-in-react-router-dom
-import { useHistory } from "react-router-dom";
+import { useHistory, Redirect, NavLink } from "react-router-dom";
 import UserContext from "./UserContext";
 import {
   Container,
@@ -12,104 +12,177 @@ import {
   Label,
   Input,
 } from "reactstrap";
+import JoblyApi from "../JoblyApi";
 
-function UserSignupForm() {
-  const INITIAL_STATE = {
-    username: "",
-    password: "",
-    first_name: "",
-    last_name: "",
-    email: "",
-  };
-  const [formData, setFormData] = useState(INITIAL_STATE);
-  const { currentUser } = useContext(UserContext);
-  const history = useHistory();
+class UserSignupForm extends React.Component {
+  static contextType = UserContext;
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      redirect: false,
+      signup: true,
+    };
 
+    this.showUserSignup = this.showUserSignup.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  const handleChange = (e) => {
+  showUserSignup() {
+    this.setState({ signup: true });
+  }
+
+  handleChange(e) {
     const { name, value } = e.target;
-    setFormData((fData) => ({
-      ...fData,
-      [name]: value,
-    }));
-  };
+    this.setState({ [name]: value });
+  }
 
-  const handleSubmit = (e) => {
+  async handleSubmit(e) {
     e.preventDefault();
-    console.debug("handleSubmit formData=", formData);
-    history.push("/");
-  };
+    console.debug("handleSubmit formData=", this.state);
+    const { username, password, firstName, lastName, email } = this.state;
+    const { signup } = this.context;
+    let token = null;
+    if(this.state.signup) {
+      try {
+        token = await JoblyApi.signupUser({ username, password, firstName, lastName, email });
+        // this.context.setUser(token);
+        this.setState({ redirect: true });
+        localStorage.setItem("token", token);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      try {
+        token = await JoblyApi.login({ username, password });
+        this.context.setUser(token);
+        this.setState({ redirect: true }); 
+        localStorage.setItem("token", token);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 
-  return (
-      <Container className="mt-5">
-        <Row className="UserSignupForm">
+  renderSignupForm() {
+    const { username, password, firstName, lastName, email } = this.state;
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <FormGroup>
+          <Label for="username"> Username </Label>
+          <Input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password"> Password </Label>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="firstName"> First Name </Label>
+          <Input
+            type="text"
+            name="firstName"
+            id="firstName"
+            value={firstName}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="lastName"> Last Name </Label>
+          <Input
+            type="text"
+            name="lastName"
+            id="lastName"
+            value={lastName}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="email"> Email </Label>
+          <Input
+            type="text"
+            name="email"
+            id="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <Button color="primary" type="submit">
+          Sign Up
+        </Button>
+        <div className="mt-3 float-right">
+        <NavLink to="/login" activeClassName="active">Login</NavLink>
+        </div>
+      </Form>
+    );
+  }
+  // renderLoginForm() {
+  //   const { username, password } = this.state;
+  //   return (
+  //     <Form onSubmit={this.handleSubmit}>
+  //       <FormGroup>
+  //         <Label for="username"> Username </Label>
+  //         <Input
+
+  //           type="text"
+  //           name="username"
+  //           id="username"
+  //           value={username}
+  //           onChange={this.handleChange}
+  //         />
+  //       </FormGroup>
+  //       <FormGroup>
+  //         <Label for="password"> Password </Label>
+  //         <Input
+  //           type="password"
+  //           name="password"
+  //           id="password"
+  //           value={password}
+  //           onChange={this.handleChange}
+  //         />
+  //       </FormGroup>
+  //       <Button color="primary" type="submit">
+  //         Login
+  //       </Button>
+  //       <div className="mt-3 float-right">
+  //       <NavLink to="/signup" activeClassName="active">Sign Up</NavLink>
+  //       </div>
+  //     </Form>
+  //   );
+  // }
+  render() {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <Container>
+      <h1>Sign Up</h1>
+        <Row>
           <Card body>
-            <h1> Sign Up </h1>
-            <Form onSubmit={handleSubmit}>
-              <FormGroup>
-                <Label for="username"> Username </Label>
-                <Input
-                  type="text"
-                  name="username"
-                  id="username"
-                  placeholder="Username"
-                  onChange={handleChange}
-                  value={formData.username}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="password"> Password </Label>
-                <Input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                  onChange={handleChange}
-                  value={formData.password}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="first_name"> First Name </Label>
-                <Input
-                  type="text"
-                  name="first_name"
-                  id="first_name"
-                  placeholder="First Name"
-                  onChange={handleChange}
-                  value={formData.first_name}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="last_name"> Last Name </Label>
-                <Input
-                  type="text"
-                  name="last_name"
-                  id="last_name"
-                  placeholder="Last Name"
-                  onChange={handleChange}
-                  value={formData.last_name}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="email"> Email </Label>
-                <Input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                  onChange={handleChange}
-                  value={formData.email}
-                />
-              </FormGroup>
-              <Button color="primary" type="submit">
-                {" "}
-                Sign Up
-              </Button>
-            </Form>
+            {this.state.signup ? this.renderSignupForm() : this.renderLoginForm()}
           </Card>
         </Row>
       </Container>
-  );
+    );
+  }
 }
+
 
 export default UserSignupForm;
