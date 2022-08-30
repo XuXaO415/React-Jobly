5eeb590e08b3a55c02844e98b6f857ce299430d8
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-29 21:03:12 -0700

    Formated and centered homepage

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -1,66 +1,75 @@
-import React, { useContext } from "react";
+import React, { useContext, Component } from "react";
 import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+
 import UserContext from "./Users/UserContext";
+import "./Homepage.css";
 
-//Show Homepage with simple welcome message
 
-function Homepage() {
-    const { currentUser } = useContext(UserContext);
-    const history = useHistory();
 
-    return (
-        <div className="Homepage">
-            <h1>Welcome to Jobly</h1>
-            <p className="lead">Your next dream job awaits.</p>
-            {currentUser  ? 
-            <h2>Welcome Back, {currentUser.first_name} || {currentUser.username}!
-            </h2>
-            : (
-                <p>
-                    <Link className="btn btn-primary" to="/login">
+// function Homepage() {
+//     const { currentUser } = useContext(UserContext);
+//     const history = useHistory();
+
+//     return (
+//         <div className="Homepage">
+//             <h1>Welcome to Jobly</h1>
+//             <p className="lead">Your next dream job awaits.</p>
+//             {currentUser  ? 
+//             <h2>Welcome Back, {currentUser.first_name} || {currentUser.username}!
+//             </h2>
+//             : (
+//                 <p>
+//                     <Link className="btn btn-primary" to="/login">
+//                         Login
+//                         </Link>
+//                     <Link className="btn btn-primary" to="/signup">
+//                         Signup
+//                         </Link>
+//                 </p>
+//             )}
+//         </div>
+//     );
+// }
+
+
+
+class Homepage extends React.Component {
+    static contextType = UserContext;
+
+    render() {
+        const { currentUser } = this.context;
+
+        return (
+            <div className="homepage">
+                <h1 className="h1-welcome">Welcome to Jobly</h1>
+                <p className="lead-paragraph">Your next dream job awaits here.</p>
+                {this.props.currentUser  ? (
+                    <h2>Welcome Back, {this.props.currentUser.first_name} || {this.props.currentUser.username}!
+                    </h2>
+                ) : (
+                    <p> 
+                    <center>
+                
+                    <Link className="btn btn-primary m-3" to="/login">
                         Login
-                        </Link>
-                    <Link className="btn btn-primary" to="/signup">
+                    </Link>
+                   
+                  
+                    <Link className="btn btn-secondary" to="/signup">
                         Signup
-                        </Link>
-                </p>
-            )}
-        </div>
-    );
+                    </Link>
+            
+                
+                    </center>
+                    </p>
+                )}
+            </div>
+        );
+
+    }
 }
     
 
-
-
-    // if(!currentUser) {
-    //     return (
-    //         <div className="Homepage">
-    //             <div className="container text-center">
-    //                 <h1>Jobly</h1>
-    //                 <p className="lead">Find your dream job</p>
-    //                 {/* <div>
-    //                     <Link to="/login" className="btn btn-primary">Login</Link>
-    //                     <Link to="/signup" className="btn btn-secondary">Signup</Link>
-    //                 </div> */}
-    //             </div>
-    //             </div>
-    //     );
-
-    // } else {
-    //     return (
-    //         <div className="Homepage">
-    //             <div className="container text-center">
-    //                 <h1>Welcome back, {currentUser.first_name} || {currentUser.username}!</h1>
-    //                 <p className="lead">Find your dream job</p>
-    //                 <div>
-    //                     <Link to="/companies" className="btn btn-primary">Companies</Link>
-    //                     <Link to="/jobs" className="btn btn-secondary">Jobs</Link>
-    //                 </div>
-    //             </div>
-    //             </div>
-    //     );
-    // }
 
 
 export default Homepage;
