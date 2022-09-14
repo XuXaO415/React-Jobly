7742ad1f169b1e845e3715b88f225dc5d59d9181
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-09-13 17:23:14 -0700

    Changed Navbar layout

--- frontend/src/Nav/Routes.jsx
+++ frontend/src/Nav/Routes.jsx
@@ -1,75 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Homepage from "../Homepage";
 import CompanyDetails from "../Companies/CompanyDetails";
 import CompanyList from "../Companies/CompanyList";
 import JobList from "../Jobs/JobList";
-import UserContext from "../Users/UserContext";
+
 import UserLoginForm from "../Users/UserLoginForm";
 import UserSignupForm from "../Users/UserSignupForm";
 import UserProfileForm from "../Users/UserProfileForm";
 
 function Routes({ login, signup }) {
-    const { currentUser } = useContext(UserContext);
-//   console.debug(
-//     "Routes rendered=", Boolean, 
-//     "currentUser=", currentUser,
-//     "login=", login,
-//     "signup=", signup
-// );
-
-    // return (
-    //         <Switch>
-    //             <Route exact path="/">
-    //                 <Homepage />
-    //             </Route>
-    //             <Route exact path="/companies">
-    //                 <CompanyList />
-    //             </Route> 
-    //             <Route exact path="/companies/:handle">
-    //                 <CompanyDetails />
-    //             </Route>
-    //             <Route exact path="/jobs">
-    //                 <JobList />
-    //             </Route>
-    //             <Route exact path="/login">
-    //                 <UserLoginForm login={login} />
-    //             </Route>
-    //             <Route exact path="/signup">
-    //                 <UserSignupForm signup={signup} />
-    //             </Route>
-    //             <Route exact path="/profile">
-    //                 <UserProfileForm />
-    //             </Route>
-    //         </Switch>
-    // );
-
-    return (
-            <Switch>
-                <Route exact path="/">
-                    <Homepage />
-                </Route>
-                <Route exact path="/login">
-                    <UserLoginForm login={login} />
-                </Route>
-                <Route exact path="/signup">
-                    <UserSignupForm signup={signup} />
-                </Route>
-                <Route exact path="/companies">
-                    <CompanyList />
-                </Route>
-                <Route exact path="/companies/:handle">
-                    <CompanyDetails />
-                </Route>
-                <Route exact path="/jobs">
-                    <JobList />
-                </Route>
-                <Route exact path="/profile">
-                    <UserProfileForm />
-                </Route>
-                <Redirect to="/" />
-                </Switch>
-    );
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+      <Route exact path="/login">
+        <UserLoginForm login={login} />
+      </Route>
+      <Route exact path="/signup">
+        <UserSignupForm signup={signup} />
+      </Route>
+      <Route exact path="/companies">
+        <CompanyList />
+      </Route>
+      <Route exact path="/companies/:handle">
+        <CompanyDetails />
+      </Route>
+      <Route exact path="/jobs">
+        <JobList />
+      </Route>
+      <Route exact path="/profile">
+        <UserProfileForm />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  );
 }
+
 
 export default Routes;
