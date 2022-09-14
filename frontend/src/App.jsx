218112920f895982303bdf6e456eeb6fa5669cf0
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-09-13 19:25:21 -0700

    Actively working on this project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,45 @@
-import React, { useState, useEffect, Component } from "react";
+import React,{useState,useEffect,Component} from "react";
 import JoblyApi from "./JoblyApi";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import Navigation from "./Nav/Navigation";
 import Routes from "./Nav/Routes";
 import UserContext from "./Users/UserContext";
 import jwt from "jsonwebtoken";
 import useLocalStorage from "./hooks/useLocalStorage";
 
-export const TOKEN_STORAGE_ID = "jobly-token";
+export const TOKEN_STORAGE_ID="jobly-token";
 
-// function App() {
+
+
 //   const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID, null);
-//   const [currentUser, setCurrentUser] = useState(null);
-//   const [isLoading, setIsLoading] = useState(false);
 
-//   console.debug("App", { token, currentUser, isLoading });
+
+  /* Make login, signup, and logout functions in the App component.
+
+By passing login, logout, and signup functions down to the login and signup forms and the navigation bar, 
+they’ll be able to call centralized functions to perform these processes.
+
+Add token as a piece of state in App, along with state for the currentUser.
+
+Create an effect triggered by a state change of the token: this should call the backend to get
+ information on the newly-logged-in user and store it in the currentUser state.
+
+Expose the current user throughout the app with a context provider. 
+
+This will make it easy to refer to the current app in navigation, on pages, and so on.
+
+This would be an excellent place to use useContext, so you can store the current user’s info high up in your hierarchy, 
+like on the App component. */
 
 function App() {
   const [token, setToken] = useLocalStorage('token', null);
   const [currentUser, setCurrentUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const [formData, setFormData] = useState({});
 
-  console.debug("App", { token, currentUser, isLoading });
-  // console.debug("App", { isLoggedIn });
+
+  console.debug("App", { token, currentUser, isLoading, isLoggedIn });
+
 
   useEffect(function updateUserInfo() {
     async function getCurrentUser() {
@@ -82,10 +97,10 @@ function App() {
         setCurrentUser(null);
   }
 
-  function updateProfile(username, data) {
+  function updateProfile(username, data={}) {
     async function updateUser() {
       try {
-        let user = await JoblyApi.updateUser(username, data);
+        let user = await JoblyApi.updateUser(username, data={});
         setCurrentUser(user);
         return { success: true, update: true };
       } catch (err) {
@@ -95,41 +110,53 @@ function App() {
     updateUser();
  }
 
+
  if (isLoading) {
    return <div>Loading...</div>;
  }
-
 
   function searchCompanies(search) {
     async function searchCompanies() {
       try {
         let companies = await JoblyApi.searchCompanies(search);
-        return { success: true, search: true };
+
+        return { success: true, search: true, companies };
       } catch (err) {
         return { success: false, search: false, err };
-      }
+      } 
     }
-    searchCompanies();
-    return { success: true };
 
-  }
+    searchCompanies();
+}
+
+
+
+
+  // function searchCompanies(search={}) {
+  //   async function searchCompanies() {
+  //     try {
+  //       let companies = await JoblyApi.searchCompanies(search={});
+  //       return { success: true, search: true };
+  //     } catch (err) {
+  //       return { success: false, search: false, err };
+  //     }
+  //   }
+  //   searchCompanies();
+  //   return { success: true };
+
+  // }
 
 
   return (
     <BrowserRouter>
-      <UserContext.Provider value={{currentUser}}>
+      <UserContext.Provider value={{currentUser, setCurrentUser }}>
 
-        <Navigation logout={logout} />
+        <Navigation logout={logout}  />
         <Routes login={login} signup={signup} updateProfile={updateProfile} searchCompanies={searchCompanies}/>
       </UserContext.Provider>
     </BrowserRouter>
   );      
 }
-
-  
-
-
-
 
 
 
