8738148f4c2f57c2ea119453b83e9ffb7f88ad83
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-09-21 20:59:55 -0700

    Stuck on navigation on current user. Does login properly, goes straight to companies

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -4,102 +4,46 @@ import UserContext from './Users/UserContext'
 import JoblyApi from './JoblyApi'
 import './Homepage.css'
 
-// class Homepage extends React.Component {
-//     static contextType = UserContext;
-
-//     render() {
-//         let currentUser  = this.context;
-//         return (
-//             <div className="Homepage App-header mt-5">
-//             <div className="container text-lg-center">
-//                 <h1 className="h1-welcome">Welcome to Jobly</h1>
-//                 <p className="lead-paragraph">Your next dream job awaits you here.</p>
-//                 {this.props.currentUser  ?
-//                     <h2>Welcome Back, {currentUser.firstName} || {this.context.username}!
-//                     </h2>
-//                     : (
-//                         <center>
-//                         <Link className="btn btn-primary m-3" to="/login">
-//                             Login
-//                         </Link>
-//                         <Link className="btn btn-primary m-3" to="/signup">
-//                             Signup
-//                         </Link>
-//                         </center>
-//                     )}
-//             </div>
-//             </div>
-//         );
-//     }
-// }
 
 // function Homepage() {
-//     const { currentUser } = useContext(UserContext);
-//     const welcome = currentUser ? (
-//         <h2>Welcome Back, {currentUser.firstName}!</h2>
-//     ) : (
-//         <center>
-//             <Link className="btn btn-primary m-3" to="/login">
-//                 Login
-//             </Link>
-//             <Link className="btn btn-primary m-3" to="/signup">
-//                 Signup
-//             </Link>
-//         </center>
-//     );
-//     return (
-//         <div className="Homepage App-header mt-5">
-//             <div className="container text-lg-center">
-//                 <h1 className="h1-welcome">Welcome to Jobly</h1>
-//                 <p className="lead-paragraph">Your next dream job awaits you here.</p>
-//                 {welcome}
-//             </div>
-//         </div>
+//   const { currentUser } = useContext(UserContext);
+//   console.debug("Homepage", "currentUser=", currentUser);
+
+
+//   return (
+//     <div className="Homepage">
+//       <div className="container text-center">
+//         <h1 className="mb-4 font-weight-bold">Jobly</h1>
+//         <p className="lead">Your next dream job awaits you here.</p>
+//         {currentUser ? (
+//           <h2>Welcome Back, {currentUser.firstName || currentUser.username}!</h2>
+//         ) : (
+//             <p>
+//               <Link className="btn btn-primary font-weight-bold m-3" to="/login">Log in</Link>
+//               <Link className="btn btn-primary font-weight-bold m-3" to="/signup">Sign up</Link>
+//             </p>
+//           )}
+//       </div>
+//     </div>
 //     );
 // }
 
-// const Homepage = () => {
-
-//     const { currentUser } = useContext(UserContext);
-
-//     return (
-//         <div className="Homepage App-header mt-5">
-//         <div className="container text-lg-center">
-//             <h1 className="h1-welcome">Welcome to Jobly</h1>
-//             <p className="lead-paragraph">Your next dream job awaits you here.</p>
-//             {currentUser ?
-//                 <h2>Welcome Back, {currentUser.firstName}!
-//                 </h2>
-//                 : (
-//                     <center>
-//                     <Link className="btn btn-primary m-3" to="/login">
-//                         Login
-//                     </Link>
-//                     <Link className="btn btn-primary m-3" to="/signup">
-//                         Signup
-//                     </Link>
-//                     </center>
-//                 )}
-//         </div>
-//         </div>
-//     );
-// }
 
 function Homepage() {
-  const { currentUser } = useContext(UserContext)
-  const [message, setMessage] = useState('Welcome to Jobly')
+  const { currentUser } = useContext(UserContext);
+  const [message, setMessage] = useState('Welcome to Jobly');
 
   useEffect(() => {
     async function getMessage() {
       if (currentUser) {
-        console.debug('Homepage', 'loadMessage', 'currentUser')
-        setMessage(`Welcome Back, ${currentUser.firstName}!`)
+        console.debug('Homepage', 'loadMessage', 'currentUser');
+        setMessage(`Welcome Back, ${currentUser.firstName || currentUser.username}!`);
       } else {
-        setMessage('Welcome to Jobly')
+        setMessage('Welcome to Jobly');
       }
     }
-    getMessage()
-  }, [currentUser])
+    getMessage();
+  }, [currentUser]);
 
   return (
     <div className="Homepage App-header mt-5">
--- frontend/src/Nav/Navigation.jsx
+++ frontend/src/Nav/Navigation.jsx
@@ -14,166 +14,127 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 import { useHistory } from 'react-router-dom'
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse'
 
-class Navigation extends React.Component {
-  static contextType = UserContext
+// class Navigation extends React.Component {
+//   static contextType = UserContext
 
-  render() {
-    const currentUser = this.context
+//   render() {
+//     const currentUser = this.context
 
+//     return (
+//       <Navbar bg="light" expand="lg">
+//         <Container fluid>
+//           <Navbar.Brand href="/">
+//             <span>
+//               <img
+//                 src={careerChoice}
+//                 width="30"
+//                 height="30"
+//                 className="d-inline-block align-top"
+//                 alt="job-image"
+//               />
+//             </span>
+//             <span>Jobly</span>
+//           </Navbar.Brand>
+//           <Navbar.Toggle aria-controls="navbarScroll" />
+//           <Navbar.Collapse id="navbarScroll">
+//             <Nav
+//               className="me-auto my-2 my-lg-0"
+//               style={{ maxHeight: '100px' }}
+//               navbarScroll
+//             >
+//               {currentUser ? (
+//                 <Navbar.Collapse className="justify-content-center">
+//                   {/* <Navbar.Text>Welcome back, {currentUser.firstName || currentUser.username}</Navbar.Text> */}
+//                   <Nav.Link href="/companies">Companies</Nav.Link>
+//                   <Nav.Link href="/jobs">Jobs</Nav.Link>
+//                   <NavDropdown title="Profile" id="basic-nav-dropdown">
+//                     <NavDropdown.Item href="/profile">
+//                       Edit profile
+//                     </NavDropdown.Item>
+//                   </NavDropdown>
+//                   <Nav.Link href="/logout">Logout</Nav.Link>
+//                 </Navbar.Collapse>
+//               ) : (
+//                 <Navbar.Collapse>
+//                   <NavDropdown title="Login" id="basic-nav-dropdown">
+//                     <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+//                     <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
+//                   </NavDropdown>
+//                 </Navbar.Collapse>
+//               )}
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//     );
+//   }
+// }
+
+
+//If someone is logged in, show their username in the navigation, along with a way to log out. 
+//If no one is logged in, show a link to the login page and a link to the signup page.
+
+function Navigation({ logout }) {
+
+  const { currentUser } = useContext(UserContext);
+  console.debug('Navigation', 'logout=', typeof logout);
+
+  function loggedInNav() {
     return (
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="/">
-            <span>
-              <img
-                src={careerChoice}
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-                alt="job-image"
-              />
-            </span>
-            <span>Jobly</span>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              {currentUser ? (
-                <Navbar.Collapse className="justify-content-center">
-                  {/* <Navbar.Text>Welcome back, {currentUser.firstName || currentUser.username}</Navbar.Text> */}
-                  <Nav.Link href="/companies">Companies</Nav.Link>
-                  <Nav.Link href="/jobs">Jobs</Nav.Link>
-                  <NavDropdown title="Profile" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/profile">
-                      Edit profile
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                  <Nav.Link href="/logout">Logout</Nav.Link>
-                </Navbar.Collapse>
-              ) : (
-                <Navbar.Collapse>
-                  <NavDropdown title="Login" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-                    <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
-                  </NavDropdown>
-                </Navbar.Collapse>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    )
+      <Nav className="me-auto">
+        <NavLink className={NavLink} to="/companies">
+          Companies
+        </NavLink>
+        <NavLink className={NavLink} to="/jobs">
+          Jobs
+        </NavLink>
+        <NavLink className={NavLink} to="/profile">
+          Profile
+        </NavLink>
+        <Link to="/" onClick={logout}>
+          Log out {currentUser.username || currentUser.first_name}
+        </Link>
+      </Nav>
+    );
   }
-}
 
-{
-  /* {!currentUser ? 
-            <NavDropdown title="Login" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-              <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
-            </NavDropdown>
-           : ( 
-            //if currentUser is true, then show companies, jobs, profile and logout links 
+  function loggedOutNav() {
+    return (
+      <Nav className="me-auto">
+        <Nav.Link as={NavLink} to="/login">
+          Login
+        </Nav.Link>
+        <Nav.Link as={NavLink} to="/signup">
+          Signup
+        </Nav.Link>
+      </Nav>
+    );
+  }
 
-            <NavDropdown title="Profile" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/profile">My Profile</NavDropdown.Item>
-              <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
-            </NavDropdown>
-          )}
-          </Nav>
-          <Nav className="ml-auto" navbarScroll>
-            <Nav.Link href="companies" className="active">Companies</Nav.Link>
-            <Nav.Link href="jobs" className="active">Jobs</Nav.Link>
-          </Nav>
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/">
+          <span>
+            <img
+              src={careerChoice}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="job-image"
+            />
+          </span>
+          <span>Jobly</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          {currentUser ? loggedInNav() : loggedOutNav()}
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    )
-  }
-}  */
+  );
 }
 
-// function Navigation({ logout }) {
-//   const currentUser = useContext(UserContext)
 
-//   function handleClick() {
-//     logout()
-//   }
-//   function loggedInNav() {
-//     return (
-//       <ul className="navbar-nav ml-auto">
-//         <li className="nav-item mr-4">
-//           <NavLink className="nav-link" to="/companies">
-//             Companies
-//           </NavLink>
-//         </li>
-//         <li className="nav-item mr-4">
-//           <NavLink className="nav-link" to="/jobs">
-//             Jobs
-//           </NavLink>
-//         </li>
-//         <li className="nav-item mr-4">
-//           <NavLink className="nav-link" to="/profile">
-//             Profile
-//           </NavLink>
-//         </li>
-//         <li className="nav-item">
-//           <button
-//             onClick={handleClick}
-//             className="nav-link btn btn-link nav-link-btn"
-//           >
-//             Logout {currentUser.username}
-//           </button>
-//         </li>
-//       </ul>
-//     )
-//   }
-//   function loggedOutNav() {
-//     return (
-//       <ul className="navbar-nav ml-auto">
-//         <li className="nav-item mr-4">
-//           <NavLink className="nav-link" to="/login">
-//             Login
-//           </NavLink>
-//         </li>
-//         <li className="nav-item mr-4">
-//           <NavLink className="nav-link" to="/signup">
-//             Signup
-//           </NavLink>
-//         </li>
-//       </ul>
-//     )
-//   }
-//   return (
-//     <nav className="Nav navbar navbar-expand-lg navbar-light bg-light">
-//       <NavLink key="home" exact to="/" className="navbar-brand">
-//         Jobly
-//       </NavLink>
-//       <button
-//         class="navbar-toggler"
-//         type="button"
-//         data-toggle="collapse"
-//         data-target="#navbarToggleExternalContent"
-//         aria-controls="navbarToggleExternalContent"
-//         aria-expanded="false"
-//         aria-label="Toggle navigation"
-//       >
-//         <span class="navbar-toggler-icon"></span>
-//       </button>
-//       <div
-//         className="collapse navbar-collapse"
-//         id="navbarToggleExternalContent"
-//       >
-//         <NavbarCollapse className="navbar-nav ml-auto">
-//           {currentUser ? loggedInNav() : loggedOutNav()}
-//         </NavbarCollapse>
-//       </div>
-//     </nav>
-//   );
-// }
 
 export default Navigation
