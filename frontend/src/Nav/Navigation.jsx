29aa464fd4f04d3622f68d1ddedbd821d0920f0d
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-09-26 18:24:43 -0700

    Working on impoving navigation functionality

--- frontend/src/Nav/Navigation.jsx
+++ frontend/src/Nav/Navigation.jsx
@@ -13,65 +13,118 @@ import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import { useHistory } from 'react-router-dom'
 
-class Navigation extends React.Component {
-  static contextType = UserContext
+//create a navbar component
+function Navigation({ logout }) {
+  const { currentUser } = useContext(UserContext)
+  const history = useHistory()
 
-  render() {
-    const logout = this.props.logout
-    const currentUser = this.context
-
-    return (
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="/">
-            <span>
-              <img
-                src={careerChoice}
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-                alt="job-image"
-              />
-            </span>
-            <span>Jobly</span>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              {currentUser ? (
-                <Navbar.Collapse className="justify-content-center">
-                  <Navbar.Text>Welcome back, {currentUser.firstName || currentUser.username}</Navbar.Text>
-                  {/* <Navbar.Text>Welcome back, {currentUser.username}!</Navbar.Text> */}
-             
-                  <Nav.Link href="/companies">Companies</Nav.Link>
-                  <Nav.Link href="/jobs">Jobs</Nav.Link>
-                  <NavDropdown title="Profile" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/profile">
-                      Edit profile
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                  {/* <Nav.Link href="/logout">Logout</Nav.Link> */}
-                <Nav.Link className="nav-link" to="/" onClick={logout}>Logout</Nav.Link>
-                </Navbar.Collapse>
-              ) : (
-                <Navbar.Collapse>
-                  <NavDropdown title="Login" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-                    <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
-                  </NavDropdown>
-                </Navbar.Collapse>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+  const handleLogin = () => {
+    history.push('/login')
   }
+
+  const handleLogout = () => {
+    logout()
+    history.push('/')
+  }
+
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={careerChoice}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          Jobly
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/companies">
+              Companies
+            </Nav.Link>
+            <Nav.Link href='/jobs'>
+              Jobs
+            </Nav.Link>
+            <Nav.Link href="/profile">
+              Profile
+            </Nav.Link>
+            {currentUser ? (
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            ) : (
+              <Nav.Link onClick={handleLogin}>
+                Login
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+
+// class Navigation extends React.Component {
+//   static contextType = UserContext
+
+//   render() {
+//     const logout = this.props.logout
+//     const currentUser = this.context
+
+//     return (
+//       <Navbar bg="light" expand="lg">
+//         <Container fluid>
+//           <Navbar.Brand href="/">
+//             <span>
+//               <img
+//                 src={careerChoice}
+//                 width="30"
+//                 height="30"
+//                 className="d-inline-block align-top"
+//                 alt="job-image"
+//               />
+//             </span>
+//             <span>Jobly</span>
+//           </Navbar.Brand>
+//           <Navbar.Toggle aria-controls="navbarScroll" />
+//           <Navbar.Collapse id="navbarScroll">
+//             <Nav
+//               className="me-auto my-2 my-lg-0"
+//               style={{ maxHeight: '100px' }}
+//               navbarScroll
+//             >
+//               {currentUser ? (
+//                 <Navbar.Collapse className="justify-content-center">
+//                   {/* <Navbar.Text>Welcome back, {currentUser.firstName || currentUser.username}</Navbar.Text> */}
+//                   <Navbar.Text>Welcome back, {currentUser.username}!</Navbar.Text>
+//                   <Nav.Link href="/companies">Companies</Nav.Link>
+//                   <Nav.Link href="/jobs">Jobs</Nav.Link>
+//                   <NavDropdown title="Profile" id="basic-nav-dropdown">
+//                     <NavDropdown.Item href="/profile">
+//                       Edit profile
+//                     </NavDropdown.Item>
+//                   </NavDropdown>
+//                   {/* <Nav.Link href="/logout">Logout</Nav.Link> */}
+//                 <Nav.Link className="nav-link" to="/" onClick={logout}>Logout</Nav.Link>
+//                 </Navbar.Collapse>
+//               ) : (
+//                 <Navbar.Collapse>
+//                   <NavDropdown title="Login" id="basic-nav-dropdown">
+//                     <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+//                     <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
+//                   </NavDropdown>
+//                 </Navbar.Collapse>
+//               )}
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//     );
+//   }
+// }
 
 
 
