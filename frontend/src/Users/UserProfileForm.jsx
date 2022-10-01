fa67ec7d4cbdf674c93c158744186045350d363e
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-09-30 20:48:10 -0700

    User profile is not working

--- frontend/src/Users/UserProfileForm.jsx
+++ frontend/src/Users/UserProfileForm.jsx
@@ -9,134 +9,249 @@ import { Form, FormGroup, Button, Input, Label, Badge } from "reactstrap";
 
 //changed from function to class. Also changed name from UserProfileForm to plain Profile
 //May change all file names 
-class Profile extends React.Component {
-  static contextType = UserContext;
+// class Profile extends React.Component {
+//   static contextType = UserContext;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       username: "",
+//       firstName: "",
+//       lastName: "",
+//       email: "",
+//       password: "",
+//     };
+
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   async handleSubmit(e) {
+//     e.preventDefault();
+//     console.debug("handleSubmit formData=", this.state);
+//     const { username, password, firstName, lastName, email } = this.state;
+//     const { updateUser } = this.context;
+//     let token = null;
+//     try {
+//       token = await JoblyApi.updateUser({
+//         username,
+//         firstName,
+//         lastName,
+//         email,
+//         password,
+//       });
+//       this.context.setUser(token);
+//       this.setState({ redirect: true });
+//       localStorage.setItem("token", token);
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   }
+
+//   handleChange(e) {
+//     const { name, value } = e.target;
+//     this.setState((st) => ({...st, [name]: value,}));
+//   }
+
+//   render() {
+//     return (
+//       <div className="col-md-6 col-lg-4 offset-md-3 offset-lg-4">
+//         <div className="Profile">
+//           <h1>Profile</h1>
+//           <Form onSubmit={this.handleSubmit}>
+//             <FormGroup>
+//               <Label for="username">Username
+//               <Badge pill bg="primary">    
+//               {/* Hi, {this.context.currentUser.username}!  */}
+//               </Badge>
+//               </Label>
+              
+              
+//               <p>
+//               <Badge pill bg="primary">
+//               {/* Hi, {this.context.currentUser.username}! */}
+//               {/* {this.context.currentUser.firstName} {this.context.currentUser.lastName} */}
+//               </Badge>
+//               </p>
+      
+//             </FormGroup>
+
+//             <FormGroup>
+//               <Label for="firstName">First Name</Label>
+//               <Input
+//                 type="text"
+//                 className="form-control"
+//                 id="firstName"
+//                 name="firstName"
+//                 value={this.state.firstName}
+//                 onChange={this.handleChange}
+//               />
+//             </FormGroup>
+
+//             <FormGroup>
+//               <Label for="lastName">Last Name</Label>
+//               <Input
+//                 type="text"
+//                 className="form-control"
+//                 id="lastName"
+//                 name="lastName"
+//                 value={this.state.lastName}
+//                 onChange={this.handleChange}
+//               />
+//             </FormGroup>
+
+//             <FormGroup>
+//               <Label for="email">Email</Label>
+//               <Input
+//                 type="email"
+//                 className="form-control"
+//                 id="email"
+//                 name="email"
+//                 value={this.state.email}
+//                 onChange={this.handleChange}
+//               />
+//             </FormGroup>
+//             <FormGroup>
+//               <Label for="password">Confirm password to make changes:</Label>
+//               <Input
+//                 type="password"
+//                 className="form-control"
+//                 id="password"
+//                 name="password"
+//                 value={this.state.password}
+//                 onChange={this.handleChange}
+//               />
+//             </FormGroup>
+//             {this.state.errors.length ? (
+//               <Alert type="danger" messages={this.state.errors} />
+//             ) : null}
+//             <Button color="primary">Save Changes</Button>
+//           </Form>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+function Profile({ updateProfile }) {
+  const currentUser = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    username: currentUser.username,
+    firstName: currentUser.firstName,
+    lastName: currentUser.lastName,
+    email: currentUser.email,
+    password: "",
+  });
+  const [formErrors, setFormErrors] = useState([]);
+
+  console.debug(
+    "Profile",
+    "currentUser=",
+    currentUser,
+    "formData=",
+    formData,
+    "formErrors=",
+    formErrors
+  );
+
+async function handleSubmit(e) {
+    e.preventDefault();
+    console.debug("handleSubmit formData=", formData);
+    let profileData = {
+      username: formData.username,
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      password: formData.password,
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  async handleSubmit(e) {
-    e.preventDefault();
-    console.debug("handleSubmit formData=", this.state);
-    const { username, password, firstName, lastName, email } = this.state;
-    const { updateUser } = this.context;
-    let token = null;
+    let username = currentUser.username;
+    let updatedUser;
     try {
-      token = await JoblyApi.updateUser({
-        username,
-        firstName,
-        lastName,
-        email,
-        password,
-      });
-      this.context.setUser(token);
-      this.setState({ redirect: true });
-      localStorage.setItem("token", token);
-    } catch (err) {
-      console.error(err);
+      updatedUser = await JoblyApi.updateUser(username, profileData);
+    } catch (errors) {
+      setFormErrors(errors);
+      return;
     }
+
+    setFormErrors([]);
+    updateProfile(updatedUser);
   }
 
-  handleChange(e) {
+
+  function handleChange(e) {
     const { name, value } = e.target;
-    this.setState((st) => ({...st, [name]: value,}));
+    setFormData(f => ({ ...f, [name]: value }));
   }
 
-  render() {
-    return (
-      <div className="col-md-6 col-lg-4 offset-md-3 offset-lg-4">
-        <div className="Profile">
-          <h1>Profile</h1>
-          <Form onSubmit={this.handleSubmit}>
-            <FormGroup>
-              <Label for="username">Username
-              <Badge pill bg="primary">    
-              {/* Hi, {this.context.currentUser.username}!  */}
-              </Badge>
-              </Label>
-              
-              
-              <p>
-              <Badge pill bg="primary">
-              {/* Hi, {this.context.currentUser.username}! */}
-              {/* {this.context.currentUser.firstName} {this.context.currentUser.lastName} */}
-              </Badge>
-              </p>
-      
-            </FormGroup>
+  // let alert = formErrors.length ? 
+  //   <Alert type="danger" messages={formErrors} />
+  // : null;
 
-            <FormGroup>
-              <Label for="firstName">First Name</Label>
-              <Input
-                type="text"
-                className="form-control"
-                id="firstName"
-                name="firstName"
-                value={this.state.firstName}
-                onChange={this.handleChange}
-              />
-            </FormGroup>
+  return (
+    <div className="col-md-6 col-lg-4 offset-md-3 offset-lg-4">
+      <div className="Profile">
+        <h1>Profile</h1>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label for="username">Username</Label>
+            <Badge pill bg="primary">
+              {UserContext.username}
+            </Badge>
+          </FormGroup>
 
-            <FormGroup>
-              <Label for="lastName">Last Name</Label>
-              <Input
-                type="text"
-                className="form-control"
-                id="lastName"
-                name="lastName"
-                value={this.state.lastName}
-                onChange={this.handleChange}
-              />
-            </FormGroup>
+          <FormGroup>
+            <Label for="firstName">First Name</Label>
+            <Input
+              type="text"
+              className="form-control"
+              id="profile-firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+            />
+          </FormGroup>
 
-            <FormGroup>
-              <Label for="email">Email</Label>
-              <Input
-                type="email"
-                className="form-control"
-                id="email"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </FormGroup>
+          <FormGroup>
+            <Label for="lastName">Last Name</Label>
+            <Input
+              type="text"
+              className="form-control"
+              id="profile-lastName"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+            />
+          </FormGroup>
 
-            <FormGroup>
-              <Label for="password">Confirm password to make changes:</Label>
-              <Input
-                type="password"
-                className="form-control"
-                id="password"
-                name="password"
-                value={this.state.password}
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-            {this.state.errors.length ? (
-              <Alert type="danger" messages={this.state.errors} />
-            ) : null}
-            <Button color="primary">Save Changes</Button>
-          </Form>
-        </div>
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input
+              type="email"
+              className="form-control"
+              id="profile-email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Confirm password to make changes:</Label>
+            <Input
+              type="password"
+              className="form-control"
+              id="profile-password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          {alert}
+          <Button color="primary">Save Changes</Button>
+        </Form>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-//Add a feature where the logged-in user can edit their profile. Make sure that when a user saves changes here, those are reflected elsewhere in the app.
-
-
-
 
 export default Profile;
