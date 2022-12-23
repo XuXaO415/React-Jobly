39651e36065714e037a8f3a67ca76976542e22bc
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-12-22 22:22:33 -0800

    Added bacground images, cleaned up welcome messages

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -1,23 +1,30 @@
-import React, { useContext, Component, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import UserContext from './Users/UserContext'
-import JoblyApi from './JoblyApi'
-import './Homepage.css'
-
+import React, { useContext, Component, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import UserContext from "./Users/UserContext";
+import JoblyApi from "./JoblyApi";
+import "./Homepage.css";
 
 function Homepage() {
   const { currentUser } = useContext(UserContext);
-  const [message, setMessage] = useState('Welcome to Jobly');
+  const [message, setMessage] = useState("Welcome to Jobly");
   console.debug("Homepage", "currentUser =", currentUser);
-  console.debug("Homepage", "message =", Boolean(message), "if true, showing message =", message );
+  console.debug(
+    "Homepage",
+    "message =",
+    Boolean(message),
+    "if true, showing message =",
+    message
+  );
 
   return (
-    <div className="Homepage App-header mt-5">
+    <div className="Homepage App-header">
       <div className="container text-lg-center">
         <h1 className="h1-welcome">Welcome to Jobly</h1>
         <p className="lead-paragraph">Your next dream job awaits you here.</p>
         {currentUser ? (
-          <h2>Welcome Back, {currentUser.firstName || currentUser.username}!</h2>
+          <h2 className="welcome-msg">
+            Welcome Back, {currentUser.firstName || currentUser.username}!
+          </h2>
         ) : (
           <center>
             <Link className="btn btn-primary m-3" to="/login">
